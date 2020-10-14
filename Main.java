39ba888cdee17fbb1c88476f47433fae16fd99ed
celer7841 <celer7841@wrdsb.ca>
2020-10-14 02:44:01 +0000

    finished

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @author celer7841 
  */
 public class Main {
 
@@ -11,5 +12,26 @@ public class Main {
   public static void main(String[] args) {
     // TODO code application logic here
     
+    examGrade(85);
+    examGrade(60);
+    examGrade(72);
+    
   }
+
+
+  public static void examGrade(int score){
+    if(score < 50){
+      System.out.println("F");
+    }else if(score < 60){
+      System.out.println("D");
+    }else if(score < 70){
+      System.out.println("C");
+    }else if(score < 80){
+      System.out.println("B");
+    }else{
+      System.out.println("A");
+    }
+  }
+  
+
 }
